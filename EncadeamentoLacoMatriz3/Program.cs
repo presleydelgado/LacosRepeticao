e2cf1027ad164fd8a004e8bc58f1763dad19cf84
Presley Bonifacio Delgado <presley.bonifacio@gmail.com>
2023-10-13 13:14:45 -0300

    encadeamento

---
+++ EncadeamentoLacoMatriz3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Digite o tamanho da matriz:");
+
+int tamanho = int.Parse(Console.ReadLine());
+
+
+for(int x = 0; x <= tamanho; x++)
+{
+    for(int y = 0; y <= tamanho; y++)
+    {
+        Console.Write("(" +x+ ","+y+")");
+    }
+    Console.WriteLine();
+}
